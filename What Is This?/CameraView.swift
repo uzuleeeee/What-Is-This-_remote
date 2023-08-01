d09468a751a5cbc2b49bf5527e84fa4887c0e9fd
Chris Lee <chris0803lee@gmail.com>
2023-08-01 15:48:52 +0900

    Added camera view

--- What Is This?/CameraView.swift
+++ What Is This?/CameraView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct SecondPage: View {
+struct CameraView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct SecondPage_Previews: PreviewProvider {
+struct CameraView_Previews: PreviewProvider {
     static var previews: some View {
-        SecondPage()
+        CameraView()
     }
 }
--- What Is This?/MainView.swift
+++ What Is This?/MainView.swift
@@ -6,17 +6,12 @@
 //
 
 import SwiftUI
+import AVFoundation
+import Vision
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            
-        }
-        .padding()
+        Text("")
     }
 }
 
