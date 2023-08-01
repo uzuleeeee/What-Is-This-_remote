790c57879f97f90be8831044f71ee439ebdedbab
Chris Lee <chris0803lee@gmail.com>
2023-08-01 11:10:58 +0900

    Initial Commit

---
+++ What Is This?/What_Is_This_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_Is_This_App.swift
+//  What Is This?
+//
+//  Created by Mac-aroni on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct What_Is_This_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
