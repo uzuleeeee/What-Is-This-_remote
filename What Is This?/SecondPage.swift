910e0fb49ccb98397ab9a0129a46384fd3c79a2f
Chris Lee <chris0803lee@gmail.com>
2023-08-01 11:15:28 +0900

    Added SecondView

---
+++ What Is This?/SecondPage.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPage.swift
+//  What Is This?
+//
+//  Created by Mac-aroni on 8/1/23.
+//
+
+import SwiftUI
+
+struct SecondPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondPage()
+    }
+}
