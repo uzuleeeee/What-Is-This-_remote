35fffaf84059eb3685379fbef1108f32518d4fb7
Chris Lee <chris0803lee@gmail.com>
2023-08-04 21:37:35 +0900

    Changed google search view to always show images

--- What Is This?/Views/GoogleSearchView.swift
+++ What Is This?/Views/GoogleSearchView.swift
@@ -6,19 +6,42 @@
 //
 
 import SwiftUI
+import Network
 
 struct GoogleSearchView: View {
     let term: String
-
+    @State private var isOnline = true
+    
     var body: some View {
-        GoogleWebView(searchTerm: term)
-            .navigationBarTitle(Text("Google Image Search"))
-            .navigationBarBackButtonHidden(true)
+        VStack {
+            if isOnline {
+                GoogleWebView(searchTerm: term)
+                    .navigationBarTitle(Text("Google Image Search"))
+            } else {
+                Text("No internet connection. Please check your Wi-Fi or mobile data settings.")
+                    .foregroundColor(.red)
+                    .padding()
+            }
+        }
+        .onAppear {
+            checkInternetConnection()
+        }
+    }
+    
+    private func checkInternetConnection() {
+        let monitor = NWPathMonitor()
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        
+        monitor.pathUpdateHandler = { path in
+            isOnline = path.status == .satisfied
+        }
+        
+        monitor.start(queue: queue)
     }
 }
 
 struct GoogleSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        GoogleSearchView(term: "Banana")
+        GoogleSearchView(term: "Apple")
     }
 }
